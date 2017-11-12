77a50f657c1661c6551bc4c9a939db34207fd2f6
DimitriyLeks <dimitriyl@DimitriLekssMBP.fios-router.home>
2017-11-12 17:47:39 -0500

    new api added

--- api.py
+++ api.py
@@ -14,6 +14,18 @@ def root():
 
     return render_template('api.html', header = d['title'], description = d['explanation'], image = d['url'])
 
+@app.route('/weather')
+def weather():
+    u = urllib2.urlopen('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22chicago%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+    url = u.geturl
+    header = u.info()
+    page = u.read()
+
+    d = json.loads(page)
+    wind_dict = d['query']['results']['channel']['wind']
+    
+    return render_template('weather.html', header = 'Wind\n', description = 'Chill: ' + wind_dict['chill'] + '\nDirection: ' + wind_dict['direction'] + '\nSpeed: ' + wind_dict['speed'] + '\n')
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
